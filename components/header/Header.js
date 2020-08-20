905140d3ec3185894d406636a8d38386c4dacc44
caleb russel <calebrussel77@gmail.com>
2020-08-20 13:15:57 +0200

    fixing navigation ui

--- components/header/Header.js
+++ components/header/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex flex-row space-x-16 border-b border-gray-400 bg-white py-3 shadow-lg justify-center">
+    <div className="max-w-2xl w-full flex flex-row space-x-12 border-b border-gray-400 bg-white py-3 shadow-lg justify-center">
       <Link href="/">
         <a className="font-semibold text-gray-700 hover:text-indigo-700 inline-block p-2 border-b hover:border-indigo-700 transition ease-in-out duration-500 border-gray-400 ">
           Home
