9be9ee81a147931bcf64f599ba5081a8ced166d5
caleb russel <calebrussel77@gmail.com>
2020-08-20 13:10:32 +0200

    adding config for purgeCss

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
 const purgecssConfig = [
-  '@fullhuman/postcss-purgecss',
+  "@fullhuman/postcss-purgecss",
   {
     // Specify the paths to all of the template files in your project
-    content: ['./src/**/*.js'],
+    content: ["./src/**/*.js"],
     // make sure css reset isnt removed on html and body
-    whitelist: ['html', 'body'],
+    whitelist: ["html", "body"],
     // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
 ];
 
 module.exports = {
-  plugins: ['tailwindcss', ...(process.env.PURGE_CSS ? [purgecssConfig] : [])],
+  plugins: ["tailwindcss"],
 };
